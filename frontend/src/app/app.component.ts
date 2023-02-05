4a59942a1ab3c8f289f6878bb40b747b61cb0cb3
Nikita <ivanovvnikita@mail.ru>
2023-02-05 18:57:58 +0500

    refactored header panel + added style

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "./services/auth-service/auth.service";
+import {from, Observable, of, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import {AuthService} from "./services/auth-service/auth.service";
 })
 export class AppComponent {
   title = 'frontend';
+  authed: Observable<boolean> = of(this.authService.isAuthenticated());
 
   constructor(private router: Router, private authService: AuthService) {
   }
